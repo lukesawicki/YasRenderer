5af3a805ba33b4ac42279b34ed46248cc9704905
lukesawicki <lukaszsa.1@gmail.com>
2024-09-13 23:35:22 +0200

    Simple drawing in to JPanel

---
+++ src/pl/lukesawicki/YasTests.java
@@ -0,0 +1,19 @@
+/**
+ * 
+ */
+package pl.lukesawicki;
+
+/**
+ * 
+ */
+public class YasTests {
+
+	/**
+	 * @param args
+	 */
+	public static void main(String[] args) {
+		// TODO Auto-generated method stub
+		System.out.println("Hello Yas Projects");
+	}
+
+}
